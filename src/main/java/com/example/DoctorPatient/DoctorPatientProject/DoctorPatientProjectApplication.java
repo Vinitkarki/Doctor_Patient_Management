1944b92700131c0d3f41f0a375ba127db6da195a
Vinit karki <vinitkarki29797@gmail.com>
2023-04-15 13:11:42 +0530

    second commit

--- src/main/java/com/example/DoctorPatient/DoctorPatientProject/DoctorPatientProjectApplication.java
+++ src/main/java/com/example/DoctorPatient/DoctorPatientProject/DoctorPatientProjectApplication.java
@@ -3,7 +3,10 @@ package com.example.DoctorPatient.DoctorPatientProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
+
 public class DoctorPatientProjectApplication {
 
 	public static void main(String[] args) {
